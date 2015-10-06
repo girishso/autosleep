42256206ed05b09dcfd856ae327ee3bf6948a7ae
Girish S <girish.sonawane@gmail.com>
2015-10-06 14:12:28 +0530

    basic proxy done

--- main.go
+++ main.go
@@ -10,13 +10,14 @@ import (
 func main() {
 
 	http.HandleFunc("/", proxy)
-	log.Fatal(http.ListenAndServe(":8088", nil))
+	log.Fatal(http.ListenAndServe(":80", nil))
 }
 
 func proxy(w http.ResponseWriter, r *http.Request) {
 
 	// change the request host to match the target
-	u, _ := url.Parse("http://127.0.0.1:8002")
+	u, _ := url.Parse("http://127.0.0.1:8080")
+	log.Println(r.URL.String())
 	proxy := http.StripPrefix("", httputil.NewSingleHostReverseProxy(u))
 	// You can optionally capture/wrap the transport if that's necessary (for
 	// instance, if the transport has been replaced by middleware). Example:
